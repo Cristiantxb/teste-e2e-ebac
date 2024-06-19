994bca3b043b412291cb6e01efceb97d4860e032
Cristian Borges <cristian_teixeirab@hotmail.com>
2024-06-19 20:29:27 -0300

    alteração teste e2e

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import produtosPage from "../support/page_objects/produtos.page";
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -10,13 +12,23 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      produtosPage.visitarUrl()
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
+      produtosPage.buscarProduto('Abominable Hoodie')
+      produtosPage.addProdutoCarrinho('XS', 'Blue', 1)
+      produtosPage.buscarProduto('Aero Daily Fitness Tee')
+      produtosPage.addProdutoCarrinho('S', 'Black', 2)
+      produtosPage.buscarProduto('Aether Gym Pant')
+      produtosPage.addProdutoCarrinho(32, 'Brown', 1)  
+      produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+      produtosPage.addProdutoCarrinho('M', 'Green', 1)
+      cy.get('.sub-title').click()  
+      cy.get('#cart .checkout').click()
+      cy.checkout('Cristian', 'Borges', 'Rua Comendador Franco, 24', 'Ribeirão Preto', '17065985', '998574669', 'ctx.b@ebac.com.br')
+      cy.get('#terms').click()
+      cy.get('#place_order').click()
+      cy.get('.woocommerce-order-details__title').should('contain', 'Detalhes do pedido')     
   });
-
-
 })
